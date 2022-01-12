4426e1d5002ea863cf1817dff9a0f63ee012135e
Joel Martin <github@martintribe.org>
2022-01-12 13:17:00 -0600

    node: automatic core file loading.
    Generate miniMAL-core.js file that exports the core lib code to
    evaluate. Load and evaluate that file automatically in the miniMAL
    executor. use the top-levle miniMAL-core.js to generate the
    web/miniMAL-core.js file with a simple sed to replace the exports with
    core_ns variable.

--- js/stepB_node.js
+++ js/stepB_node.js
@@ -36,7 +36,7 @@ function EVAL(ast, env, seq, f, el) {
       // apply
       if (ast[0] == "def") {        // update current environment
         return env[ast[1]] = EVAL(ast[2], env)
-      } else if (ast[0] == "~") {  // mark as macro
+      } else if (ast[0] == "~") {   // mark as macro
         return Object.assign(EVAL(ast[1], env), {M: 1}) // mark as macro
       } else if (ast[0] == "`") {   // quote (unevaluated)
         return ast[1]
@@ -60,7 +60,7 @@ function EVAL(ast, env, seq, f, el) {
         }, {A: [ast[2], env, ast[1]]})
   
       // TCO cases
-      } else if (ast[0] == "let") {        // new environment with bindings
+      } else if (ast[0] == "let") { // new environment with bindings
         env = Object.create(env)
         ast[1].map((e,i) => i%2 ? env[ast[1][i-1]] = EVAL(ast[1][i], env) : 0)
         ast = ast[2]
